67ff0bdeac4377c97e787e68a0b1c83f08690f5a
Gomes <816461@corpaa.aa.com>
2016-07-24 15:34:46 +0100

    refactoring

---
+++ app/scripts/third-party/polyfill.js
@@ -0,0 +1,29 @@
+(function() {
+  /**
+   * @file Defines polyfills to add features not supported by the browser.
+   *
+   * NOTE: Javascript variables have two scopes: function and global.
+   *
+   * Notice that we are declaring self invoking anonymous function
+   * expressions; that is, "(function() {...}).();". This programming practice
+   * allows any variable declared within the scope of the annonymous function
+   * to be encapsulated and only visible within the scope and execution of
+   * that function. Therefore, it prevents the pollution of the global scope.
+   *
+   * @author Rubens Gomes
+   */
+
+  // place this function in "strict" operating context to prevent "unsafe"
+  // actions from happening, and to throw more exceptions.
+  'use strict';
+
+  // add support for "startWith"
+  if (!String.prototype.startsWith)
+  {
+    String.prototype.startsWith = function(searchString, position) {
+      position = position || 0;
+      return this.substr(position, searchString.length) === searchString;
+    };
+  }
+
+})();
